3c40d754d2d8adc9d7db5aca616aabf782b353c4
Copenhagen1822 <142807130+Copenhagen1822@users.noreply.github.com>
2025-03-10 09:42:47 -0300

    Add files via upload

--- JUAN/Site-criativo-main/JuanCesar/site-acessivel-projeto-inicial/script.js
+++ JUAN/Site-criativo-main/JuanCesar/site-acessivel-projeto-inicial/script.js
@@ -1,7 +1,20 @@
-document.addEventListener('DOMContentLoaded',function(){
-const aumentaFonteBotao = this.document.getElementById('aumentar-fonte');
+document.addEventListener('DOMContentLoaded', function(){
+ const aumentaFonteBotao = document.getElementById('aumentar-fonte');
+ const diminuirFonteBotao = document.getElementById('aumentar-fonte');
 
-let tamanhoAtualFonte = 1;
+ let tamanhoAtualFonte = 1;
+
+ aumentaFonteBotao.addEventListener('click', function(){
+    tamanhoAtualFonte += 0.1;
+    document.body.style.fontSize = '${tamanhoAtualFonte}rem'
+
+ })
+
+ dominuiFonteBotao.addEventListener('click', function(){
+    tamanhoAtualFonte -= 0.1;
+    document.body.style.fontSize = '${tamanhoAtualFonte}rem'
+
+ })
 
 
 })
